d86d91798836a1e83d84e4a6014814b426e163dd
DESKTOP-H77RVV6\ycy12 <ycy1208@gmail.com>
2022-06-23 19:59:45 +0100

    Initialised Player class

---
+++ Player.py
@@ -0,0 +1,14 @@
+class Player:
+    def __init__(self, name) -> None:
+        self.__numPieces = 12
+        self.__name = name
+    @property
+    def numPieces(self):
+        return self.__numPieces
+
+    def decreaseNumPieces(self, n): #?
+        self.__numPieces -= n
+
+    @property
+    def getName(self):
+        return self.__name
