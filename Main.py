1df314608a857a25c6c68738cd0eb8e9bdcf2661
DESKTOP-H77RVV6\ycy12 <ycy1208@gmail.com>
2022-06-28 12:51:45 +0100

    Made moves of distance 1 and 2

--- Main.py
+++ Main.py
@@ -1,8 +1,10 @@
 from Ui import Gui, Terminal
 from sys import argv
+from Game import Game
+from Player import Player
 
 
 if __name__ == "__main__":
     if len(argv) == 1:
-        ui = Terminal()
+        ui = Terminal(Game(Player("Joe", "o"), Player("Daniel", "x")))
     ui.run()
