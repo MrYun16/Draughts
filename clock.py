8b00ef07cf5e0d44aeacf8aaa9004a25366efc58
DESKTOP-H77RVV6\ycy12 <ycy1208@gmail.com>
2022-11-12 14:03:40 +0000

    created a new clock class

--- clock.py
+++ clock.py
@@ -2,46 +2,57 @@ from msilib.schema import Error
 from tkinter import *
 from time import *
 import threading
-class TimeError(Exception):
-    pass
 
-class Timer(threading.Thread):
-    def __init__(self, initialTimeInSec, timeString) -> None:
-        threading.Thread.__init__(self)
-        self.timeString = timeString
-        self.currentTime = initialTimeInSec
-        newHr, newMins, newSec = self.getHrMinSec(self.currentTime)
-        self.updateVariables(newHr, newMins, newSec)
-    def run(self):
-       
-        while self.currentTime > 0:
-            #print(self.currentTime)
-            sleep(1)
-            newHr, newMins, newSec = self.getHrMinSec(self.currentTime)
-            self.updateVariables(newHr, newMins, newSec)
-            self.currentTime -= 1
+
+class Clock:
+    def __init__(self, startTime, timeString, win) -> None: # startTime is in demiSeconds
+        print("initialised")
+        self.__timeString = timeString # timeString in StringVar()
+        self.__timeString.set(startTime)
+        self.__currentTimeInDemiSec = startTime
+        self.__win = win
+        self.__notStopped = True
+
+    def start(self):
+        self.decrement()
+
+    def decrement(self):
+        if self.__notStopped:
+            newTime = self.getTimeInString()
+            self.__timeString.set(newTime)
+            self.__currentTimeInDemiSec -= 1
+            self.__win.after(10, self.decrement())
+
+    def stop(self):
+        self.__notStopped = False
+
+    
+
+    def getTimeInString(self): #time is in centi seconds
+        hrs = str(self.__currentTimeInDemiSec//360000)
+        mins = str((self.__currentTimeInDemiSec%360000)//6000)
+        sec = str((self.__currentTimeInDemiSec%6000)//100)
+        if len(hrs) == 1:
+            hrs="0"+hrs
+        if len(mins) == 1:
+            mins="0"+mins
+        if len(sec) == 1:
+            sec="0"+sec
+        if self.__currentTimeInDemiSec <= 1500:
+            centiSec = str(self.__currentTimeInDemiSec%100)
+            if len(centiSec) == 1:
+                centiSec="0"+centiSec
+            return ":".join(hrs, mins, sec, centiSec)
+        return ":".join(hrs, mins, sec)
         
-    def updateTimeVar(self, newHr, newMins, newSec):
-        time = [newHr, newMins, newSec]
-        for i, unit in enumerate(time):
-            if unit <= 9:
-                time[i] = "0"+str(unit)
-            else:
-                time[i] = str(time[i])
-        self.timeString.set(":".join(time))
 
-    def getHrMinSec(self, time):
-        hr, mins, sec = time//3600, (time%3600)//60, (time%3600)%60
-        return hr, mins, sec 
 
-    def join(self):
-        threading.Thread.join(self)
-        raise TimeError("Ran out of time")
-
-timer = Timer(1)
-timer.start()
-try:
-    timer.join()
-except TimeError as e:
-    print(e)
-
+"""
+    def getTimeInDemiSec(timeString):
+        t = timeString.get().split(":")
+        hrs, mins, sec = int(t[0]), int(t[1]), int(t[2])
+        if len(t) == 4:
+            centiSec = int(t[3])
+            return hrs*360000+mins*6000+sec*100+centiSec
+        return hrs*360000+mins*6000+sec*100
+"""
